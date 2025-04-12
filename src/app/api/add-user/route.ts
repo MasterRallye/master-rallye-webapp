958e301ab67d5d046dd70a12e54f563a795ca088
Master Rallye <masterrallye@gmail.com>
2025-04-12 12:37:20 +0200

    feat: création de la route API /api/add-user

--- src/app/api/add-user/route.ts
+++ src/app/api/add-user/route.ts
@@ -2,21 +2,22 @@ import { NextResponse } from 'next/server'
 import { supabase } from '@/lib/supabaseClient'
 
 export async function POST(req: Request) {
-  const form = await req.json()
+  const body = await req.json()
+
+  const { first_name, last_name, email, phone } = body
 
   const { data, error } = await supabase.from('users').insert([
     {
-      first_name: form.first_name,
-      last_name: form.last_name,
-      email: form.email,
-      phone_number: form.phone,
+      first_name,
+      last_name,
+      email,
+      phone_number: phone,
     },
   ])
 
   if (error) {
-    console.error('Erreur Supabase :', error)
     return NextResponse.json({ error: error.message }, { status: 500 })
   }
 
-  return NextResponse.json({ data }, { status: 200 })
+  return NextResponse.json({ message: 'Utilisateur ajouté', data }, { status: 200 })
 }
