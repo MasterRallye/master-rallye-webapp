eecaefbf99e6ea3b68868e83db5d4ebb870392e9
Master Rallye <masterrallye@gmail.com>
2025-04-12 13:13:45 +0200

    fix: remove unused import to fix build error

--- src/app/add-user/page.tsx
+++ src/app/add-user/page.tsx
@@ -3,7 +3,7 @@
 export const dynamic = "force-dynamic" // ← Peut venir juste après
 
 import { useState } from 'react'
-import { supabase } from '@/lib/supabaseClient'
+//import { supabase } from '@/lib/supabaseClient'
 
 // console.log('SUPABASE_URL =', process.env.NEXT_PUBLIC_SUPABASE_URL)
 // console.log('SUPABASE_KEY =', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
